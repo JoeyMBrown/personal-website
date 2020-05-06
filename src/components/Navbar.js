62e641314f1ec76a9d8a33bc0ba1cb8d0b177fae
Joey_M_Brown <joey.ma.brown@gmail.com>
2020-05-06 12:14:06 -0400

    Added global CSS stylesheet, added gatsby-browser.js to import stylesheet.  Changed all in-line styles in Navbar to class/id's.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,54 +6,25 @@ import { Link } from 'gatsby'
 
 const Navbar = () => {
         return (
-        <div style={{
-            width: '100%',
-            backgroundColor: '#333333'
-        }}>
-            <ul style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
-            listStyle: 'none'
-        }}>
-                <li style={{
-                    flexGrow: '.01'
-                }}>
-                    <Link style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                    }} to="/">
+        <div id="Navbar">
+            <ul id="LinkList">
+                <li>
+                    <Link className="IndividualLinks active" to="/">
                         Home
                     </Link>
                 </li>
-                <li style={{
-                    flexGrow: '.01'
-                }}>
-                    <Link style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                    }} to="/">
+                <li>
+                    <Link className="IndividualLinks" to="/">
                         Portfolio
                     </Link>
                 </li>
-                <li style={{
-                    flexGrow: '.01',
-                    color: 'white'
-                }}>
-                    <Link style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                    }} to="/">
+                <li>
+                    <Link className="IndividualLinks" to="/">
                         Blog
                     </Link>
                 </li>
-                <li style={{
-                    flexGrow: '.01'
-                }}>
-                    <Link style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                    }} to="/">
+                <li>
+                    <Link className="IndividualLinks" to="/">
                         Resume 
                     </Link>
                 </li>
